bd4c30c91be97430905fe83073f40c8912834679
Aldi Fajrin <aldisma2pyk@gmail.com>
2017-01-31 16:31:22 +0700

    randoms frontend ads

--- app/Libraries/AdsHelper.php
+++ app/Libraries/AdsHelper.php
@@ -5,7 +5,6 @@ use App\Models\AdEdit;
 use App\Models\AdStatus;
 use App\Models\AdMeta;
 use App\Models\SystemLog;
-use App\Quotation;
 
 function getAds($status)
 {
@@ -27,17 +26,19 @@ function getAdsApprove($status)
 
 function getActiveAds($order = false)
 {
-	$rowOrder = $order;
-	$ads = Ad::where(function($q) use($rowOrder){
-		$now_date = date('Y-m-d H:i:s');
-		$q->where('status', 3);
-		$q->where('show_date', '<=', $now_date);
-		$q->where('expired_date', '>=', $now_date);
+	$ads = Ad::orderByRaw('RAND()')->take(10)->get();
 
-		if ($rowOrder === true) {
-			$q->orderByRaw('RAND()');
-		}
-	})->paginate(10);
+	// $rowOrder = $order;
+	// $ads = Ad::where(function($q) use($rowOrder){
+	// 	$now_date = date('Y-m-d H:i:s');
+	// 	$q->where('status', 3);
+	// 	$q->where('show_date', '<=', $now_date);
+	// 	$q->where('expired_date', '>=', $now_date);
+
+	// 	if ($rowOrder === true) {
+	// 		$q->orderByRaw('RAND()');
+	// 	}
+	// })->paginate(10);
 
 	return $ads;
 }
